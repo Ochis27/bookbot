c5fc7d0b68316fd293db0c8ada11fcf045b06933
Ochis27 <ochispaulcatalin27@gmail.com>
2025-12-12 01:06:50 +0200

    made changes in order to count the letters from words that are not duplicated"

--- stats.py
+++ stats.py
@@ -1,6 +1,11 @@
 def number_of_words(text: str) -> str:
   """function to count the number of words in a given text."""
-  num_words = 0
+  alpahabet = 'abcdefghijklmnopqrstuvwxyz'
+  letters = {}
+  text = text.lower()
   for word in text.split():
-    num_words += 1
-  print(f"Found {num_words} total words")
+    for char in word:
+      if char in alpahabet:
+        letters[char] = letters.get(char, 0) + 1
+  print(letters)
+
